748628956906d89f2375a421c90bc9f7605c2713
Adam Fejes <adam.fejes@tel.io>
2025-08-04 09:54:31 +0200

    edit TODOs

--- composeApp/src/androidMain/kotlin/hu/adamfejes/zenmotes/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/hu/adamfejes/zenmotes/MainActivity.kt
@@ -13,13 +13,22 @@ import hu.adamfejes.zenmotes.ui.theme.ZenMotesTheme
     * TODOs
     * Enable Logging only in debug builds
     * Color picker selected state + redesign?
-    * Decorated obstacles, built up from sand with shading
+    *   Colors are randomly changing. Next one is show on the top in a colorbar and warns about the change
+    * Go full screen, immersive mode
     * Fix obstacle generator to not generate obstacles on each other
     * Settings, shared preferences
     *   Theme
     *   Game mode: Zen / Time attack?
     *   High Score
     * Scoring
+    *   Scores are adding when obstacles are destroyed.
+    *   Scores are deducted when obstacles are leaving the screen without being destroyed.
+    *   Score is shown in the top center of the screen.
+    *   Additional score is animating into the center from the left
+    *   Deducted score is animating out of the center to the right
+    *   Timer is counting under the score
+    *   The longer you can keep the score above 0, the higher rank you get
+    *   When score is below 0 for 5 secs, game is over
     * Continuously changing sand color?
     * More obstacle designs?
  */
