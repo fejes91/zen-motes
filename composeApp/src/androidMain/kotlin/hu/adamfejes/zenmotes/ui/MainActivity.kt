aec7e57cc1b031dc64f1bc211b4ca0cfbac36b3a
Adam Fejes <adam.fejes@tel.io>
2025-08-08 21:59:11 +0200

    update TODOs

--- composeApp/src/androidMain/kotlin/hu/adamfejes/zenmotes/ui/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/hu/adamfejes/zenmotes/ui/MainActivity.kt
@@ -15,7 +15,7 @@ import org.koin.core.context.startKoin
     * ZenMotes
     * TODOs
     * Enable Logging only in debug builds
-    * Color picker selected state + redesign?
+    * Color picker redesign?
     *   Colors are randomly changing. Next one is show on the top in a colorbar and warns about the change
     * Go full screen, immersive mode
     * Play sounds for: sea, sand, obstacle destruction, score change?
@@ -25,11 +25,6 @@ import org.koin.core.context.startKoin
     *   Game mode: Zen / Time attack?
     *   High Score
     * Scoring
-    *   Scores are adding when obstacles are destroyed.
-    *   Scores are deducted when obstacles are leaving the screen without being destroyed.
-    *   Score is shown in the top center of the screen.
-    *   Additional score is animating into the center from the left
-    *   Deducted score is animating out of the center to the right
     *   Timer is counting under the score
     *   The longer you can keep the score above 0, the higher rank you get
     *   When score is below 0 for 5 secs, game is over
