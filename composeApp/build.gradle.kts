70dfc48d996c32647960276e0da54c6ef0b7bc22
Adam Fejes <adam.fejes@tel.io>
2025-09-25 21:26:33 +0200

    bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "hu.adamfejes.castleblaster"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.1"
 
     }
     packaging {
