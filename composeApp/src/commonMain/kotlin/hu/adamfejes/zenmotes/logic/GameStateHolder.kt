b3ddf377e1ecc78c5d5b2ed11da007044ee8ba42
adam.fejes <fejes91@gmai.com>
2025-10-25 08:24:46 +0200

    fix sound manager on pause

--- composeApp/src/commonMain/kotlin/hu/adamfejes/zenmotes/logic/GameStateHolder.kt
+++ composeApp/src/commonMain/kotlin/hu/adamfejes/zenmotes/logic/GameStateHolder.kt
@@ -1,12 +1,14 @@
 package hu.adamfejes.zenmotes.logic
 
+import hu.adamfejes.zenmotes.service.SoundManager
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class GameStateHolder(
     private val sandColorManager: SandColorManager,
     private val sandGridHolder: SandGridHolder,
-    private val scoreHolder: ScoreHolder
+    private val scoreHolder: ScoreHolder,
+    private val soundManager: SoundManager
 ) {
     private val isPausedState = MutableStateFlow(false)
     val isPaused: Flow<Boolean> = isPausedState
@@ -20,6 +22,7 @@ class GameStateHolder(
         scoreHolder.pauseTimer()
         sandColorManager.pause()
         sandGridHolder.sandGrid?.onPause()
+        soundManager.onPause()
     }
 
     fun onResume() {
@@ -28,6 +31,7 @@ class GameStateHolder(
         scoreHolder.resumeTimer()
         sandColorManager.resume()
         sandGridHolder.sandGrid?.onResume()
+        soundManager.onResume()
     }
 
     fun restart() {
