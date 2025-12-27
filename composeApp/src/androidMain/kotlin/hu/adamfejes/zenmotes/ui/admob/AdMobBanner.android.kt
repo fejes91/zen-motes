77bfb772ccbb6d0974a59aa9c30532cc2c514e22
adam.fejes <fejes91@gmai.com>
2025-12-27 09:45:41 +0100

    re-enable admob ads

--- composeApp/src/androidMain/kotlin/hu/adamfejes/zenmotes/ui/admob/AdMobBanner.android.kt
+++ composeApp/src/androidMain/kotlin/hu/adamfejes/zenmotes/ui/admob/AdMobBanner.android.kt
@@ -8,14 +8,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.google.android.ump.ConsentInformation
 import com.google.android.ump.UserMessagingPlatform
 import hu.adamfejes.zenmotes.utils.Logger
-
-//import androidx.compose.ui.viewinterop.AndroidView
-//import com.google.android.gms.ads.AdRequest
-//import com.google.android.gms.ads.AdSize
-//import com.google.android.gms.ads.AdView
 
 @Composable
 actual fun AdMobBanner(unitId: String, modifier: Modifier) {
@@ -30,17 +29,16 @@ actual fun AdMobBanner(unitId: String, modifier: Modifier) {
         Logger.d("AdMobBanner", "Can request ads: $canRequestAds")
     }
 
-    // todo turn on ads after publishing
-//    if (canRequestAds) {
-//    AndroidView(
-//        modifier = modifier,
-//        factory = { context ->
-//            AdView(context).apply {
-//                setAdSize(AdSize.BANNER)
-//                adUnitId = unitId
-//                loadAd(AdRequest.Builder().build())
-//            }
-//        }
-//    )
-//    }
+    if (canRequestAds) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                adUnitId = unitId
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+    }
 }
