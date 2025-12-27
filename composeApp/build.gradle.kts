9e6ff713b7054320aa5df2e11b4b53eb4fe445d8
adam.fejes <fejes91@gmai.com>
2025-12-27 14:40:48 +0100

    bump version to 1.1.0(14)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,8 +77,8 @@ android {
         applicationId = "hu.adamfejes.castleblaster"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.1"
+        versionCode = 14
+        versionName = "1.1.0"
         manifestPlaceholders["ADMOB_APP_ID"] = project.findProperty("admob.appid") ?: ""
     }
     packaging {
