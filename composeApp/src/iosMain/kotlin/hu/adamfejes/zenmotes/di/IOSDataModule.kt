883038182bd17803d7dfa5899970b832d944c09d
adam.fejes <fejes91@gmai.com>
2026-01-02 19:56:10 +0100

    add main menu theme music for ios

--- composeApp/src/iosMain/kotlin/hu/adamfejes/zenmotes/di/IOSDataModule.kt
+++ composeApp/src/iosMain/kotlin/hu/adamfejes/zenmotes/di/IOSDataModule.kt
@@ -4,7 +4,9 @@ import hu.adamfejes.zenmotes.config.KoinNames
 import hu.adamfejes.zenmotes.data.createDataStore
 import hu.adamfejes.zenmotes.service.AnalyticsService
 import hu.adamfejes.zenmotes.service.IOSAnalyticsService
+import hu.adamfejes.zenmotes.service.IOSMusicManager
 import hu.adamfejes.zenmotes.service.IOSSoundManager
+import hu.adamfejes.zenmotes.service.MusicManager
 import hu.adamfejes.zenmotes.service.SoundManager
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -12,6 +14,7 @@ import org.koin.dsl.module
 val iosDataModule = module {
     single { createDataStore() }
     single<SoundManager> { IOSSoundManager() }
+    single<MusicManager> { IOSMusicManager() }
     single<AnalyticsService> { IOSAnalyticsService() }
     single(named(KoinNames.ADMOB_UNIT_ID)) { "" }
 }
